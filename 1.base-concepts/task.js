2eb4f2ed430b7f5db6b00a850a7517bd49f8d109
Ali Khossamalddin <ali_milan86@mail.ru>
2026-07-10 18:12:39 +0300

    Решены задания 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,12 +6,14 @@ function solveEquation(a, b, c) {
   const d = b ** 2 - 4 * a * c;
 
     if (d < 0) {
+      
     return arr;
   }
 
     if (d === 0) {
     const root = -b / (2 * a);
     arr.push(root);
+
     return arr;
   }
 
@@ -20,12 +22,19 @@ function solveEquation(a, b, c) {
     const root1 = (-b + sqrtD) / (2 * a);
     const root2 = (-b - sqrtD) / (2 * a);
     arr.push(root1, root2);
+
     return arr;
   }
-  
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+    const percentDecimal = percent / 100;
+    const loanBody = amount - contribution;
+    const monthlyRate = percentDecimal / 12;
+    const monthlyPayment = loanBody * (monthlyRate + monthlyRate / ((1 + monthlyRate) ** countMonths - 1));
+    const totalPayment = monthlyPayment * countMonths;
+
+    return Math.round(totalPayment * 100) / 100;
+  }
