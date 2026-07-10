e39f5459f72e40b28ccb75ebaa9cf7aa2aa52a67
Ali Khossamalddin <ali_milan86@mail.ru>
2026-07-10 17:24:31 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+
+  const d = b ** 2 - 4 * a * c;
+
+    if (d < 0) {
+    return arr;
+  }
+
+    if (d === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+    return arr;
+  }
+
+    if (d > 0) {
+    const sqrtD = Math.sqrt(d);
+    const root1 = (-b + sqrtD) / (2 * a);
+    const root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+    return arr;
+  }
   
   return arr;
 }
